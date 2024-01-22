3ae09440d8acaac13deb8da5e22414fff367f01a
bangundwir <hendrabangundwir@gmail.com>
2024-01-22 13:12:09 +0000

    update

--- index.ts
+++ index.ts
@@ -67,7 +67,7 @@ async function scrapeInitialData(search: string) {
 
 // Replace 'searchTerm' with the actual search term you want to use
 const searchTerm =
-    "Rafi dzaky setya pratama Malang";
+  "Rafi dzaky setya pratama Malang";
 scrapeInitialData(searchTerm);
 
 // // const mName = faker.persopersonNaull); // Generates a random full name
